195012f6de3899a1b5fcd0b4f5c36f2ad2075cd0
Arya Deshmukh <aryadeshmukh12@gmail.com>
2024-06-21 12:19:44 +0530

    more routing options

--- hello_flask.py
+++ hello_flask.py
@@ -1,7 +1,24 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<html><body><h1>Hello World!</h1></body></html>"
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+@app.route('/user/<string:username>')
+def show_user_profile(username: str) -> None:
+    return f'User: {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id: int) -> None:
+    return f'Post {escape(post_id)}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath: str) -> None:
+    return f'Subpath {escape(subpath)}'
