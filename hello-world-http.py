c373088654d49bb1061feca70c85514439b4ac6e
Arya Deshmukh <aryadeshmukh12@gmail.com>
2024-06-21 14:51:41 +0530

    routing on basic http server

--- hello-world-http.py
+++ hello-world-http.py
@@ -1,12 +1,35 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse
+from markupsafe import escape
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        parsed_path = urlparse(self.path).path
+        path_components = parsed_path.split('/')
+
+        text = b""
+        if len(path_components) <= 1:
+            text = 'Index Page'
+        elif path_components[1] == 'hello':
+            text = 'Hello World'
+        elif len(path_components) >= 3:
+            match path_components[1]:
+                case 'user':
+                    text = f'User: {escape(path_components[2])}'
+                case 'post':
+                    text = f'Post {escape(path_components[2])}'
+                case 'path':
+                    text = f'Subpath {"/".join([escape(component) for component in path_components[2:]])}'
+                case _:
+                    text = 'No page found'
+        else:
+            text = 'No page found'
+
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-        self.wfile.write(b"<html><body><h1>Hello World</h1></body></html>")
+        self.wfile.write(f'<html><body><h1>{text}</h1></body></html>'.encode('utf-8'))
     
 def run(server_class=HTTPServer, handler_class=MyHandler, port = 8000):
     server_address = ('', port)
