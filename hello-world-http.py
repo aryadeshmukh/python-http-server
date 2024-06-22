82ab509fc72c680f141969f039599f407df5b275
Arya Deshmukh <aryadeshmukh12@gmail.com>
2024-06-22 18:08:58 +0530

    functionaize GET request handling

--- hello-world-http.py
+++ hello-world-http.py
@@ -2,72 +2,55 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse
 from markupsafe import escape
 
-def write_to_wfile(handler: BaseHTTPRequestHandler, text: str) -> None:
+class ResponseInfo:
     '''
-    Formats text to be displayed on webpage. Should be called as response to client GET request.
-    
-    Keyword arguments:
-    handler -- handler that is processing GET request
-    text -- text to be displayed on webpage
-    '''
-    handler.wfile.write(f'<html><body><h1>{text}</h1></body></html>'.encode('utf-8'))
+    Response Information for GET requests.
 
-def index(handler: BaseHTTPRequestHandler) -> None:
-    '''
-    Displays appropriate message on webpage when no path is specified.
-    
-    Keyword arguments:
-    handler -- handler that is processing GET request
-    '''
-    write_to_wfile(handler, 'Index Page') 
+    ...
 
-def hello(handler: BaseHTTPRequestHandler) -> None:
+    Attributes
+    ----------
+    code : int
+        http response code
+    content_type : str
+        content type
+    body : str
+        message to be displayed
     '''
-    Displays 'Hello World' on webpage when path is /hello.
+    def __init__(self, code: int, content_type: str, body: str) -> None:
+        self.code = code
+        self.content_type = content_type
+        self.body = body
 
-    Keyword arguments:
-    handler -- handler that is processing GET request
-    '''
-    write_to_wfile(handler, 'Hello World')
+def handle_root() -> ResponseInfo:
+    '''Specifies response information for root url.'''
+    return ResponseInfo(200, 'text/plain', 'Index Page')
 
-def show_user_profile(handler: BaseHTTPRequestHandler, username: str) -> None:
-    '''
-    Displays username when path is in the form /user/<username>.
-    
-    Keyword arguments:
-    handler -- handler that is processing GET request
-    username -- username to be displayed
-    '''
-    write_to_wfile(handler, f'User: {escape(username)}')
+def handle_hello() -> ResponseInfo:
+    '''Specifies response information for url path /hello.'''
+    return ResponseInfo(200, 'text/html', '<html><body><h1>Hello World</h1></body></html>')
 
-def show_post(handler: BaseHTTPRequestHandler, post_id: int) -> None:
+def handle_user(username: str) -> ResponseInfo:
     '''
-    Displays post_id when path is in the form of /post/<post_id>
+    Specifies repsonse information for url path /user/<username>.
 
     Keyword arguments:
-    handler -- handler that is processing GET request
-    post_id -- post id number to be displayed
+    username -- username of user to be handled
     '''
-    write_to_wfile(handler, f'Post: {escape(post_id)}')
+    return ResponseInfo(200, 'text/plain', f'User: {escape(username)}')
 
-def show_subpath(handler: BaseHTTPRequestHandler, subpath: str) -> None:
+def handle_path(subpath: str) -> ResponseInfo:
     '''
-    Displays subpath when path is in the form of /path/<subpath>
+    Specifies response information for url path /path/<subpath>.
 
     Keyword arguments:
-    handler -- handler that is processing GET request
-    subpath -- rest of the path to be displayed
+    subpath -- subpath of the url path
     '''
-    write_to_wfile(handler, subpath)
+    return ResponseInfo(200, 'text/plain', f'Subpath {escape(subpath)}')
 
-def not_valid_path(handler: BaseHTTPRequestHandler) -> None:
-    '''
-    Displays appropriate message on webpage for all undefined cases.
-    
-    Keyword arguments:
-    handler -- handler that is processing GET request
-    '''
-    write_to_wfile(handler, 'No page found')
+def handle_404() -> ResponseInfo:
+    '''Specifies response information for page not found.'''
+    return ResponseInfo(404, 'text/plain', 'Page not found')
 
 class MyHandler(BaseHTTPRequestHandler):
     '''
@@ -88,34 +71,37 @@ class MyHandler(BaseHTTPRequestHandler):
         If no path is specified, 'Index Page' is displayed.
         If path is /hello, 'Hello World' is displayed.
         If path is in the form of /user/<username>, 'User: <username>' is displayed.
-        If path is in the form of /post/<post_id>, 'Post <post_id>' is displayed.
         If path is in the form of /path/<subpath>, 'Subpath <subpath>' is displayed.
         If none of the above are true then 'No page found' is displayed.
         '''
-        parsed_path = urlparse(self.path).path
-        path_components = parsed_path.split('/')
+        routes = {
+            "" : handle_root,
+            "hello" : handle_hello,
+            "user" : handle_user,
+            "path" : handle_path
+        }
 
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        try:
+            parsed_path = urlparse(self.path).path.strip('/')
+            path_components = parsed_path.split('/')
+
+            if len(path_components) == 1:
+                response_func = routes[path_components[0]]
+                response = response_func()
+            elif len(path_components) == 2:
+                response_func = routes[path_components[0]]
+                response = response_func(path_components[1])
+            else:
+                response_func = routes[path_components[0]]
+                response = response_func("/".join(path_components[1:]))  
+        except Exception:
+            response = handle_404()
+
+        self.send_response(response.code)
+        self.send_header('Content-type', response.content_type)
         self.end_headers()
+        self.wfile.write(bytes(response.body, 'utf-8'))
 
-        if len(path_components) <= 1:
-            index(self)
-        elif path_components[1] == 'hello':
-            hello(self)
-        elif len(path_components) >= 3:
-            match path_components[1]:
-                case 'user':
-                   show_user_profile(self, path_components[2])
-                case 'post':
-                    show_post(self, path_components[2])
-                case 'path':
-                    show_subpath(self, "/".join([escape(component) for component in path_components[2:]]))
-                case _:
-                    not_valid_path(self)
-        else:
-            not_valid_path(self)
-    
 def run(server_class=HTTPServer, handler_class=MyHandler, port = 8000):
     '''Runs the HTTP Server using the custom handler.'''
     server_address = ('', port)
